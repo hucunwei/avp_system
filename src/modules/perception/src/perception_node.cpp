70106f2093396367641b2552efd88bf5de45324d
hucunwei <56469660+hucunwei@users.noreply.github.com>
2025-04-04 10:05:05 +0800

    Add files via upload
    AVP Project

---
+++ src/modules/perception/src/perception_node.cpp
@@ -0,0 +1,7 @@
+#include "ros/ros.h"
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "perception_node");
+    ros::NodeHandle nh("~");
+
+}
---
+++ src/modules/planning/src/planning_node.cpp
@@ -0,0 +1,7 @@
+#include "ros/ros.h"
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "planning_node");
+    ros::NodeHandle nh("~");
+
+}
