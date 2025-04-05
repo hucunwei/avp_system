3b9cdd34894579873893eb06c2cc3e51f750ab58
Deliang Ye <deliang@foxbots.com>
2025-04-05 18:23:12 -0400

    init check in

--- src/modules/perception/src/perception_node.cpp
+++ src/modules/perception/src/perception_node.cpp
@@ -1,15 +1,52 @@
+#include "perception.h"
+
 #include "ros/ros.h"
 #include <iostream>
 #include <string>
+#include <geometry_msgs/Vector3Stamped.h>
+#include <rosbag/bag.h>
+#include <rosbag/view.h>
+
+#include <message_filters/subscriber.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <message_filters/time_synchronizer.h>
+#include <boost/bind.hpp>
+
+#include <opencv2/opencv.hpp>
+#include <sensor_msgs/Image.h>
+
+
+bool save_ipm = true;
+std::string seg = "";
 
 int main(int argc, char **argv) {
-    ros::init(argc, argv, "perception_node");
-    ros::NodeHandle nh("~");
+  ros::init(argc, argv, "perception_node");
+  ros::NodeHandle nh("~");
 
-    std::string camera_topic;
-    nh.param("topics/camera", camera_topic, std::string("/camera")); 
-    std::cout << camera_topic << std::endl;
+  bool is_label = false;
+  if (is_label) {
+    seg = "_seg";
+  }
+  std::string front_topic("/camera/front" + seg);
+  std::string back_topic("/camera/back" + seg);
+  std::string left_topic("/camera/left" + seg);
+  std::string right_topic("/camera/right" + seg);
 
-    ros::spin();
+  message_filters::Subscriber<sensor_msgs::CompressedImage> front_sub(nh, front_topic, 100);
+  message_filters::Subscriber<sensor_msgs::CompressedImage> back_sub(nh, back_topic, 100);
+  message_filters::Subscriber<sensor_msgs::CompressedImage> left_sub(nh, left_topic, 100);
+  message_filters::Subscriber<sensor_msgs::CompressedImage> right_sub(nh, right_topic, 100);
+  typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::CompressedImage, sensor_msgs::CompressedImage,
+    sensor_msgs::CompressedImage, sensor_msgs::CompressedImage> sync_pol;
+  message_filters::Synchronizer<sync_pol> sync(sync_pol(10), front_sub, back_sub, left_sub, right_sub);
 
+  CPerception perception(is_label, save_ipm);
+  sync.registerCallback(boost::bind(&CPerception::AddImage, &perception, _1, _2, _3, _4));
+  ros::Subscriber imu_sub = nh.subscribe<sensor_msgs::Imu>(
+    "/imu", 100, boost::bind(&CPerception::AddImu, &perception, _1));
+  ros::Subscriber gps_sub = nh.subscribe<nav_msgs::Odometry>(
+    "/gps_odom", 100, boost::bind(&CPerception::AddGps, &perception, _1));
+
+  ros::spin();
+  return 0;
 }
